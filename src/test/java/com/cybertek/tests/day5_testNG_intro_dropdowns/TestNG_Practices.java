b4632bacdb783d808a4170f4986db82c863e9bc2
jmvalmeria <jmvalmeria@gmail.com>
2020-09-13 15:27:33 -0400

    Assertion 02

--- src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Practices.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Practices.java
@@ -3,6 +3,7 @@ package com.cybertek.tests.day5_testNG_intro_dropdowns;
 import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -16,6 +17,9 @@ public class TestNG_Practices {
     public void setUpMethod(){
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
+
+        //Wait until the page is loaded for the given time
+        //It will up to 15 seconds. If page is loaded that it will continue
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
     }
 
@@ -29,6 +33,12 @@ public class TestNG_Practices {
         String expectTitle = "Google";
         String actualTitle = driver.getTitle();
         Assert.assertEquals(actualTitle,expectTitle, "Title are not matching!");
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws InterruptedException{
+        Thread.sleep(3000);
+        driver.close();
     }
 
 }
