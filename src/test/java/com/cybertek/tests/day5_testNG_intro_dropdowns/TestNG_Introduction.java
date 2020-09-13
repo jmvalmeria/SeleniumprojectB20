8acc665fd8981d3591f4740b43774596b12cfc25
jmvalmeria <jmvalmeria@gmail.com>
2020-09-13 12:31:45 -0400

    Update 02

---
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
@@ -0,0 +1,11 @@
+package com.cybertek.tests.day5_testNG_intro_dropdowns;
+
+import org.testng.annotations.Test;
+
+public class TestNG_Introduction {
+
+    @Test //TestNG anotation
+    public void test1(){
+        System.out.println("Test1 is running...");
+    }
+}
