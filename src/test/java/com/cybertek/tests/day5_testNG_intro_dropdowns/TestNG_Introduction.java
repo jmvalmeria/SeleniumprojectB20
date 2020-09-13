36b3411a62975bf8baf22d963cab70a9afe5e86f
jmvalmeria <jmvalmeria@gmail.com>
2020-09-13 15:19:00 -0400

    Assertion

--- src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Introduction.java
@@ -1,10 +1,17 @@
 package com.cybertek.tests.day5_testNG_intro_dropdowns;
 
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestNG_Introduction {
+
+    @BeforeClass
+    public void setUpClass(){ //It runs only 1 at the beginning of class, it is the first method that runs, we usually use it to set
+        System.out.println("Before class is running...");
+    }
 
     @BeforeMethod //It runs 1 before each Test method is running
     public void setUpMethod(){
@@ -24,5 +31,18 @@ public class TestNG_Introduction {
     @Test
     public void test2(){
         System.out.println("Test2 is running...");
+    }
+
+    @Test
+    public void test3(){
+        String str1 = "TestNG";
+        String str2 = "TestNG";
+        String str3 = "asdfasdf";
+
+        Assert.assertTrue(str1.equals(str2));
+
+        Assert.assertTrue(str1.equals(str3),"Str1 is not equal to Str3.");
+
+        Assert.assertEquals(str1,str2, "Failure message just in case str1 is not equals to str2"); //Using another assertion method
     }
 }
---
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Practices.java
@@ -0,0 +1,34 @@
+package com.cybertek.tests.day5_testNG_intro_dropdowns;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestNG_Practices {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void google_title_verification(){
+
+        //1. Go to google
+        driver.get("http://www.google.com");
+
+        //2. Verify title is google
+        String expectTitle = "Google";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle,expectTitle, "Title are not matching!");
+    }
+
+}
