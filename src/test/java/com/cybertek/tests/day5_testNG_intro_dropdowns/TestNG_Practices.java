80e357e205d7684ed1246d5437c976688015acf9
jmvalmeria <jmvalmeria@gmail.com>
2020-09-13 15:34:39 -0400

    Assetion 03

--- src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Practices.java
+++ src/test/java/com/cybertek/tests/day5_testNG_intro_dropdowns/TestNG_Practices.java
@@ -1,7 +1,10 @@
 package com.cybertek.tests.day5_testNG_intro_dropdowns;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -21,19 +24,30 @@ public class TestNG_Practices {
         //Wait until the page is loaded for the given time
         //It will up to 15 seconds. If page is loaded that it will continue
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        driver.get("http://www.google.com");
     }
 
     @Test
     public void google_title_verification(){
-
         //1. Go to google
-        driver.get("http://www.google.com");
-
         //2. Verify title is google
         String expectTitle = "Google";
         String actualTitle = driver.getTitle();
         Assert.assertEquals(actualTitle,expectTitle, "Title are not matching!");
     }
+
+    @Test
+    public void google_search_title_verification(){
+
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("apple" + Keys.ENTER);
+
+        String expectTitle = "apple";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle,expectTitle, "Title are not matching!");
+    }
+
 
     @AfterMethod
     public void tearDownMethod() throws InterruptedException{
